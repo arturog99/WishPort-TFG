c732fdd114730b7b5855aeb7cda296500fa5afa1
Sergio <font622@gmail.com>
2026-03-10 18:07:42 +0100

    Configuración inicial del proyecto

---
+++ backend/src/main/java/com/wishport/backend/WishportApiApplication.java
@@ -0,0 +1,13 @@
+package com.wishport.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishportApiApplication.class, args);
+	}
+
+}
